7a82e566dd526a498db1437cfcd054b295d272a2
Ariv-Fernandes <129577782+Ariv-Fernandes@users.noreply.github.com>
2023-09-09 13:05:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,64 @@
-from random import *
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Number of arms
+num_arms = 5
 
-def gaussian_filter(kernel_size, sigma=1, muu=0):
-    x, y = np.meshgrid(np.linspace(-1, 1, kernel_size),
-                       np.linspace(-1, 1, kernel_size))
-    dst = np.sqrt(x ** 2 + y ** 2)
+# True action values (unknown to the agent)
+reward_list = np.random.normal(0, 1, num_arms)
 
-    # lower normal part of gaussian
-    normal = 1 / (2.0 * np.pi * sigma ** 2)
+# Number of runs
+num_runs = 50
 
-    # Calculating Gaussian filter
-    return np.exp(-((dst - muu) ** 2 / (2.0 * sigma ** 2))) * normal
+# Number of time steps
+num_steps = 1000
 
+# Epsilon values to test
+epsilons = [0, 0.1, 0.01]
 
-kernel_size = 5
-gaussian = gaussian_filter(kernel_size)
+# Initialize variables to store results
+average_rewards = np.zeros((len(epsilons), num_runs, num_steps))
 
-i = 0
-new_list = [i for i in range(50)]
-t_avg = [[0] * 50] * 3
+# Run the epsilon-greedy algorithm for each epsilon value
+for e, epsilon in enumerate(epsilons):
+    for run in range(num_runs):
+        # Initialize action-value estimates
+        action_values = np.zeros(num_arms)
 
+        # Initialize counts for each action
+        num = np.zeros(num_arms)
 
-def epsilon(eps):
-    reward_list = gaussian
-    sum = [0, 0, 0, 0, 0, 0]
-    num = [0, 0, 0, 0, 0, 0]
-    avg = [0, 0, 0, 0, 0, 0]
+        # Initialize array to store rewards for each time step
+        rewards = np.zeros(num_steps)
 
-    l = [0] * 5
-    d = 0
+        for step in range(num_steps):
+            # Epsilon-greedy action selection
+            if np.random.random() < epsilon:
+                # Explore: Choose a random arm
+                arm = np.random.choice(num_arms)
+            else:
+                # Exploit: Choose the arm with the highest estimated value
+                arm = np.argmax(action_values)
+            # Get the reward from the selected arm (sample from a normal distribution)
+            reward = np.random.normal(reward_list[arm], 1)
+            # Update action counts
+            num[arm] += 1
 
-    def closest():
-        for i in range(5):
-            l[i] = abs(avg[i] - avg[5])
-        d = l.index(min(l))
-        return l.index(min(l))
+            # Update action-value estimate using sample-average method
+            action_values[arm] += (reward - action_values[arm]) / num[arm]
+            rewards[step] = reward
 
-    def exploration():
-        arm = randint(0, 4)
-        r = reward_list[arm][randint(0, 4)]
-        sum[arm] = sum[arm] + r
-        sum[5] += r
-        num[arm] = num[arm] + 1
-        num[5] += 1
-        avg[arm] = round(sum[arm] / num[arm], 2)
-        avg[5] = round(sum[5] / num[5], 2)
-        change(eps)
+        # Store the average rewards for this run
+        average_rewards[e, run] = np.cumsum(rewards) / (np.arange(num_steps) + 1)
 
-    def change(eps):
-        flag = 0
-        if d == closest():
-            flag = 1
-        if flag == 1:
-            eps = eps / 2
-        else:
-            eps = eps * 2
+# Calculate and plot the average rewards for each epsilon value
+plt.figure(figsize=(10, 6))
+for e, epsilon in enumerate(epsilons):
+    avg_rewards_overall = np.mean(average_rewards[e], axis=0)
+    plt.plot(avg_rewards_overall, label=f"Epsilon = {epsilon}")
 
-    def exploitation(i):
-        r = reward_list[i][randint(0, 4)]
-        sum[i] = sum[i] + r
-        num[i] = num[i] + 1
-        avg[i] = round(sum[i] / num[i], 2)
-        change(eps)
-
-    for i in range(1000):
-        a = random()
-        if a <= eps:
-            exploitation(closest())
-        else:
-            exploration()
-    return avg[5]
-
-
-for i in range(50):
-    t_avg[0][i] = epsilon(0.4)
-    t_avg[1][i] = epsilon(0)
-    t_avg[2][i] = epsilon(0.8)
-
-xpoints = np.array(new_list)
-y1 = np.array(t_avg[0])
-y2 = np.array(t_avg[1])
-y3 = np.array(t_avg[2])
-plt.plot(xpoints, y1)
-plt.plot(xpoints, y2)
-plt.plot(xpoints, y3)
+plt.xlabel("Time Steps")
+plt.ylabel("Average Reward")
+plt.title("Epsilon-Greedy Method for K-Armed Bandit (50 Runs)")
+plt.legend()
 plt.show()
