d7a9f4d327b535cbc37b043116eea6807af7fe64
Ariv-Fernandes <129577782+Ariv-Fernandes@users.noreply.github.com>
2023-11-05 03:31:12 +0530

    Update line_following.py

--- line_following.py
+++ line_following.py
@@ -1,38 +1,25 @@
+from sim_api import AgentHost
 import numpy as np
 
+base_path = r"/Users/arivf/PycharmProjects/pythonProject/Eklavya_ctRL/ctrl-sim/"
+host = AgentHost(base_path + r"shr/obs", base_path + r"shr/act")
+
+# Use host.get_action_from_sim and host.pass_observation_to_sum to interact
+# with the simulator. Write your code below.
+
+
 MAX_ANGLE = 180  # Maximum turning angle
-MAX_PWM = 10  # Maximum PWM value
+MAX_PWM = 1  # Maximum PWM value
 l1, l2, l3, l4, l5 = 5, 5, 5, 5, 5
 # Assigning Rewards
-q_values = np.zeros((l1, l2, l3, l4, l5, 36))
+q_values = np.zeros((5, 5, 5, 5, 5, 36))
 reward_list = np.full((l1, l2, l3, l4, l5), -100)
 
 if (l2 == 0 and l3 == 0) and (l5 == 0 or l1 == 0 or l4 == 0):
     reward_list[l1, l2, l3, l4, l5] = -1
-actions = [x for x in range(181) if x % 5 == 0]
+actions = [x for x in range(-90,91) if x % 5 == 0]
 # Number of runs
 num_runs = 4000
-# Number of time steps
-# num_steps = 1000
-
-
-# function for terminal state:
-def if_terminal_state(cl1, cl2, cl3, cl4, cl5):
-    if reward_list[cl1, cl2, cl3, cl4, cl5] != -1:
-        return True
-    else:
-        return False
-
-
-# # function to choose a random non-terminal starting location:
-# def get_starting_location():
-#     # current_row_index = np.random.randint(envi_rows)
-#     # current_colomn_index = np.random.randint(envi_colomns)
-#     # while if_terminal_state(current_row_index, current_colomn_index):
-#     #     current_row_index = np.random.randint(envi_rows)
-#     #     current_colomn_index = np.random.randint(envi_colomns)
-#     # return current_row_index, current_colomn_index
-#     pass
 
 
 # epsilon greedy algorithm to choose next action
@@ -43,20 +30,25 @@ def get_next_action(cl1, cl2, cl3, cl4, cl5, eps):
         return np.random.randint(35)  # random action
 
 
+def get_observation_from_sim():
+    lsa_readings = host.get_observation_from_sim()
+    lsa_readings //= 52
+    return int(lsa_readings[0]), int(lsa_readings[1]), int(lsa_readings[2]), int(lsa_readings[3]), int(lsa_readings[4])
+
+
 def pass_action_to_sim(angle):
-    angle = max(0, min(angle, MAX_ANGLE))
+
 
     # Calculate the PWM values based on the linear mapping
     pwm_scale = MAX_PWM / MAX_ANGLE
-    pwm_left = angle * pwm_scale
-    pwm_right = -angle * pwm_scale  # Assuming opposite direction for the right wheel
+    pwm_left = 1-angle * pwm_scale
+    pwm_right = 1+angle * pwm_scale  # Assuming opposite direction for the right wheel
+    pwm = np.array((pwm_left, pwm_right), dtype=np.float64)
+    host.pass_action_to_sim(-pwm)
+    print(pwm)
 
-    return pwm_left, pwm_right
 
 
-def get_observation_from_sim():
-    # the 5 LSA readings are divided into buckets of 51 values for readings [0,255]
-    pass
 
 
 # next location based on previous action
@@ -97,22 +89,25 @@ for episode in range(num_runs):
     # row_index, column_index = get_starting_location()
 
     # continue taking actions (i.e., moving) until we reach a terminal state
-    while not if_terminal_state(l1, l2, l3, l4, l5):
-        # choose which action to take (i.e., where to move next)
-        action_index = get_next_action(l1, l2, l3, l4, l5, epsilon)
 
-        # perform the chosen action, and transition to the next state (i.e., move to the next location)
-        ol1, ol2, ol3, ol4, ol5 = l1, l2, l3, l4, l5  # store the old LSA readings
-        l1, l2, l3, l4, l5 = get_next_location(l1, l2, l3, l4, l5, action_index)
+    # choose which action to take (i.e., where to move next)
+    l1, l2, l3, l4, l5 = get_observation_from_sim()
 
-        # receive the reward for moving to the new state, and calculate the temporal difference
-        reward = reward_list[l1, l2, l3, l4, l5]
-        old_q_value = q_values[ol1, ol2, ol3, ol4, ol5, action_index]
-        temporal_difference = reward + (discount_factor * np.max(q_values[l1, l2, l3, l4, l5])) - old_q_value
+    action_index = get_next_action(l1, l2, l3, l4, l5, epsilon)
+    ol1, ol2, ol3, ol4, ol5 = l1, l2, l3, l4, l5
+    pass_action_to_sim(actions[action_index])
+    # perform the chosen action, and transition to the next state (i.e., move to the next location)
+      # store the old LSA readings
 
-        # update the Q-value for the previous state and action pair
-        new_q_value = old_q_value + (learning_rate * temporal_difference)
-        q_values[ol1, ol2, ol3, ol4, ol5, action_index] = new_q_value
+
+    # receive the reward for moving to the new state, and calculate the temporal difference
+    reward = reward_list[l1, l2, l3, l4, l5]
+    old_q_value = q_values[ol1, ol2, ol3, ol4, ol5, action_index]
+    temporal_difference = reward + (discount_factor * np.max(q_values[l1, l2, l3, l4, l5])) - old_q_value
+
+    # update the Q-value for the previous state and action pair
+    new_q_value = old_q_value + (learning_rate * temporal_difference)
+    q_values[ol1, ol2, ol3, ol4, ol5, action_index] = new_q_value
 
 print('Training complete!')
 # pygame.init()
