d8c471fcbdcaa57088b74c599e4958f41e96922e
Ariv-Fernandes <129577782+Ariv-Fernandes@users.noreply.github.com>
2023-11-04 21:41:01 +0530

    Update line_following.py

--- line_following.py
+++ line_following.py
@@ -1,17 +1,19 @@
 import numpy as np
 
-l1, l2, l3, l4, l5 = 255, 255, 255, 255, 255
+MAX_ANGLE = 180  # Maximum turning angle
+MAX_PWM = 10  # Maximum PWM value
+l1, l2, l3, l4, l5 = 5, 5, 5, 5, 5
 # Assigning Rewards
-q_values = np.zeros((l1, l2, l3, l4, l5, 3))
+q_values = np.zeros((l1, l2, l3, l4, l5, 36))
 reward_list = np.full((l1, l2, l3, l4, l5), -100)
-reward_list[177, 177, 177, 177, 177] = 100
+
 if (l2 == 0 and l3 == 0) and (l5 == 0 or l1 == 0 or l4 == 0):
     reward_list[l1, l2, l3, l4, l5] = -1
-actions = ["straight", "left", "right"]
+actions = [x for x in range(181) if x % 5 == 0]
 # Number of runs
 num_runs = 4000
-# Number of time step
-num_steps = 1000
+# Number of time steps
+# num_steps = 1000
 
 
 # function for terminal state:
@@ -22,15 +24,15 @@ def if_terminal_state(cl1, cl2, cl3, cl4, cl5):
         return False
 
 
-# function to choose a random non-terminal starting location:
-def get_starting_location():
-    # current_row_index = np.random.randint(envi_rows)
-    # current_colomn_index = np.random.randint(envi_colomns)
-    # while if_terminal_state(current_row_index, current_colomn_index):
-    #     current_row_index = np.random.randint(envi_rows)
-    #     current_colomn_index = np.random.randint(envi_colomns)
-    # return current_row_index, current_colomn_index
-    pass
+# # function to choose a random non-terminal starting location:
+# def get_starting_location():
+#     # current_row_index = np.random.randint(envi_rows)
+#     # current_colomn_index = np.random.randint(envi_colomns)
+#     # while if_terminal_state(current_row_index, current_colomn_index):
+#     #     current_row_index = np.random.randint(envi_rows)
+#     #     current_colomn_index = np.random.randint(envi_colomns)
+#     # return current_row_index, current_colomn_index
+#     pass
 
 
 # epsilon greedy algorithm to choose next action
@@ -38,40 +40,50 @@ def get_next_action(cl1, cl2, cl3, cl4, cl5, eps):
     if np.random.random() < eps:  # best action
         return np.argmax(q_values[cl1, cl2, cl3, cl4, cl5])
     else:
-        return np.random.randint(3)  # random action
+        return np.random.randint(35)  # random action
+
+
+def pass_action_to_sim(angle):
+    angle = max(0, min(angle, MAX_ANGLE))
+
+    # Calculate the PWM values based on the linear mapping
+    pwm_scale = MAX_PWM / MAX_ANGLE
+    pwm_left = angle * pwm_scale
+    pwm_right = -angle * pwm_scale  # Assuming opposite direction for the right wheel
+
+    return pwm_left, pwm_right
+
+
+def get_observation_from_sim():
+    # the 5 LSA readings are divided into buckets of 51 values for readings [0,255]
+    pass
 
 
 # next location based on previous action
 def get_next_location(cl1, cl2, cl3, cl4, cl5, action_index):
-    nl1, nl2, nl3, nl4, nl5 = cl1, cl2, cl3, cl4, cl5
-    if actions[action_index] == 'straight' and cl2 == 0 and cl3 == 0 and cl5 == 0:
-        # t.forward(5)
-        pass
-    elif actions[action_index] == 'right' and cl2 == 0 and cl3 == 0 and cl4 == 0:
-        # t.right(90)
-        # t.forward(5)
-        pass
-    if actions[action_index] == 'left' and cl1 == 0 and cl2 == 0 and cl3 == 0:
-        # t.left(90)
-        # t.forward(5)
-        pass
-    return nl1, nl2, nl3, nl4, nl5
+    if actions[action_index] == 90 and cl2 == 0 and cl3 == 0 and cl5 == 0:
+        pass_action_to_sim(actions[action_index])
+    elif 90 < actions[action_index] <= 180 and cl2 == 0 and cl3 == 0 and cl4 == 0:
+        pass_action_to_sim(actions[action_index])
+    if 90 > actions[action_index] >= 0 == cl1 and cl2 == 0 and cl3 == 0:
+        pass_action_to_sim(actions[action_index])
+    return get_observation_from_sim()
 
 
 # function for shortest path:
-def get_shortest_path(sl1, sl2, sl3, sl4, sl5):
-    if if_terminal_state(sl1, sl2, sl3, sl4, sl5):
-        return []
-    else:  # if legal starting location
-        cl1, cl2, cl3, cl4, cl5 = sl1, sl2, sl3, sl4, sl5
-        shortest_path = [[cl1, cl2, cl3, cl4, cl5]]
-        while not if_terminal_state(cl1, cl2, cl3, cl4, cl5):
-            # take the best action
-            action_index = get_next_action(cl1, cl2, cl3, cl4, cl5, 1)
-            # move to the next location on the path, and add the new location to the list
-            cl1, cl2, cl3, cl4, cl5 = get_next_location(cl1, cl2, cl3, cl4, cl5, action_index)
-            shortest_path.append([cl1, cl2, cl3, cl4, cl5])
-        return shortest_path
+# def get_shortest_path(sl1, sl2, sl3, sl4, sl5):
+#     if if_terminal_state(sl1, sl2, sl3, sl4, sl5):
+#         return []
+#     else:  # if legal starting location
+#         cl1, cl2, cl3, cl4, cl5 = sl1, sl2, sl3, sl4, sl5
+#         shortest_path = [[cl1, cl2, cl3, cl4, cl5]]
+#         while not if_terminal_state(cl1, cl2, cl3, cl4, cl5):
+#             # take the best action
+#             action_index = get_next_action(cl1, cl2, cl3, cl4, cl5, 1)
+#             # move to the next location on the path, and add the new location to the list
+#             cl1, cl2, cl3, cl4, cl5 = get_next_location(cl1, cl2, cl3, cl4, cl5, action_index)
+#             shortest_path.append([cl1, cl2, cl3, cl4, cl5])
+#         return shortest_path
 
 
 # define training parameters
@@ -103,3 +115,55 @@ for episode in range(num_runs):
         q_values[ol1, ol2, ol3, ol4, ol5, action_index] = new_q_value
 
 print('Training complete!')
+# pygame.init()
+# screen = pygame.display.set_mode((800, 500))
+# pygame.display.set_caption("Line Following Bot")
+# clock = pygame.time.Clock()
+# surface = pygame.Surface((800, 500))
+# surface.fill((52, 52, 52))
+# ellipse_center = (400, 250)
+# ellipse_radius_x = 300
+# ellipse_radius_y = 200
+# bot = pygame.image.load("bott.png").convert()
+# bot_rect = bot.get_rect(midbottom=(400, 475))
+# angle,speed=0,2
+# bot_rect.centerx, bot_rect.centery = ellipse_center[0] + ellipse_radius_x, ellipse_center[1]
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
+#
+#     screen.blit(surface, (0, 0))
+#
+#     pygame.draw.ellipse(screen, 'White', (ellipse_center[0] - ellipse_radius_x, ellipse_center[1] - ellipse_radius_y,
+#                                           ellipse_radius_x * 2, ellipse_radius_y * 2), 10)
+#     # angle -= 1*speed
+#     # if angle <= -360:
+#     #     angle = 0
+#     # bot_rect.centerx = ellipse_center[0] + ellipse_radius_x * math.cos(math.radians(angle))
+#     # bot_rect.centery = ellipse_center[1] + ellipse_radius_y * math.sin(math.radians(angle))
+#
+#     keys = pygame.key.get_pressed()
+#     if keys[pygame.K_UP]:
+#         bot_rect.centery -= 10 * speed
+#
+#     if keys[pygame.K_DOWN]:
+#         bot_rect.centery += 10 * speed
+#
+#     if keys[pygame.K_LEFT]:
+#         angle -= 1 * speed
+#         bot_rect.centerx = ellipse_center[0] + ellipse_radius_x * math.cos(math.radians(angle))
+#         bot_rect.centery = ellipse_center[1] + ellipse_radius_y * math.sin(math.radians(angle))
+#     if keys[pygame.K_RIGHT]:
+#         angle += 1 * speed
+#         bot_rect.centerx = ellipse_center[0] + ellipse_radius_x * math.cos(math.radians(angle))
+#         bot_rect.centery = ellipse_center[1] + ellipse_radius_y * math.sin(math.radians(angle))
+#     if pygame.key.get_pressed():
+#         screen.blit(pygame.transform.rotate(bot, 90 - angle), bot_rect)
+#     else:
+#         screen.blit(bot,bot_rect)
+#     pwm_1 = bot_rect.topleft
+#     pwm_2 = bot_rect.bottomleft
+#     pygame.display.update()
+#     clock.tick(60)
